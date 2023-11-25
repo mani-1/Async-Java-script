fb28128b03fbcdafc6a106ce33d59d951683afe6
mani <manikandan.tms88@gmail.com>
2023-11-25 11:53:40 +0530

    handled callback with for error and response

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,33 +1,24 @@
-const request =  new XMLHttpRequest()
 
+function getTodos(callback) {
+  const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', () => {
-  if(request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText)
-  } else if (request.readyState === 4) {
-    console.log('Oops! sry cannot able to fecth data')
+  request.addEventListener('readystatechange', () => {
+    if(request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText)
+    } else if (request.readyState === 4) {
+      callback('Oops, sry not able to fetch data')
+    }
+  })
+
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/1')
+  request.send()
+}
+
+getTodos((err, data) => {
+  console.log('callback fired')
+  if(err) {
+    console.log(err)
+  } else {
+    console.log(data)
   }
 })
-
-// Ready state 0 before open call
-
-// Ready state 1
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-// Ready state 2
-request.send()
-
-// Ready state 3 - downloading, got the partial data
-
-// Ready state 4 - Full downloaded data
-
-
-/**
-
-  Status code
-
-  Range 100 < 200  information response
-  Range 200 < 300  Successful response
-  Range 300 < 400  Redirection message
-  Range 400 < 500  Erorr from client 
-  Range 500        Error from server
- */
