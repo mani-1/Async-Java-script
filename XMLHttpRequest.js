5d71a274b23c6a9a223b161d3e541c9ed84a2a1e
mani <manikandan.tms88@gmail.com>
2023-11-25 12:54:25 +0530

    handling xmlhttp request with promise

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,43 +1,25 @@
 
-function getTodos(url, callback) {
-  const request = new XMLHttpRequest()
+function getTodos(url) {
 
-  request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200) {
-      // convert json for reald js  array of object
-      const data = JSON.parse(request.responseText)
-      callback(undefined, data)
-    } else if (request.readyState === 4) {
-      callback('Oops, sry not able to fetch data')
-    }
-  })
-
-  request.open('GET', url)
-  request.send()
-}
-
-
-// Promise example
-
-function getSomething () {
-  return new Promise((resolve, reject) => {
-    // fetch something
-    resolve('Some data')
-    // reject('Some error')
+ return  new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest()
+    
+    request.addEventListener('readystatechange', () => {
+      if(request.readyState === 4 && request.status === 200) {
+        // convert json for reald js  array of object
+        const data = JSON.parse(request.responseText)
+        resolve(data)
+      } else if (request.readyState === 4) {
+        reject('Oops, sry not able to fetch data')
+      }
+    })
+    
+    request.open('GET', url)
+    request.send()
   })
 }
 
-// One way of calling resolve and reject as first and second param
-getSomething().then((data) => {
-  console.log(data)
-},
-(err) => {
-  console.log(err)
-}
-)
-
-// Efficient way of calling resolve and reject using then and catch block
-getSomething().then((data) => {
+getTodos('./todos/todos1.json').then((data) => {
   console.log(data)
 }).catch((err) => {
   console.log(err)
